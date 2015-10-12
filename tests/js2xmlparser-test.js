36679f43013ad8d796f6f62c224a6eb2c718cf07
Frank Cornelis <info@e-contract.be>
2015-10-12 09:51:48 +0200

    Browser POST XML signature

--- tests/js2xmlparser-test.js
+++ tests/js2xmlparser-test.js
@@ -1,6 +1,31 @@
 "use strict";
 
 var js2xmlparser = require("js2xmlparser");
+var xmlCrypto = require('xml-crypto');
+
+function SecurityTokenReferenceKeyInfoProvider(tokenIdentifier) {
+    this.tokenIdentifier = tokenIdentifier;
+
+    this.getKeyInfo = function (key, prefix) {
+        var securityTokenReferenceData = {
+            "@": {
+                "xmlns:wsse": "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+            },
+            "wsse:Reference": {
+                "@": {
+                    "ValueType": "http://docs.oasis-open.org/ws-sx/ws-secureconversation/200512/sct",
+                    "URI": this.tokenIdentifier
+                }
+            }
+        };
+        var result = js2xmlparser("wsse:SecurityTokenReference", securityTokenReferenceData, {
+            declaration: {
+                include: false
+            }
+        });
+        return result;
+    };
+}
 
 exports['test js2xmlparser'] = function (test) {
     var data = {
@@ -26,5 +51,18 @@ exports['test js2xmlparser'] = function (test) {
     };
     var result = js2xmlparser("async:PendingRequest", data);
     console.log(result);
+    var signature = new xmlCrypto.SignedXml();
+    signature.signingKey = "1234";
+    signature.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#hmac-sha1";
+    signature.addReference("/", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/2001/10/xml-exc-c14n#"],
+            "http://www.w3.org/2000/09/xmldsig#sha1", "", "", "", true);
+    signature.keyInfoProvider = new SecurityTokenReferenceKeyInfoProvider("a token identifier");
+    signature.computeSignature(result, {
+        prefix: "ds",
+        location: {
+            reference: "dss:OptionalInputs"
+        }
+    });
+    console.log(signature.getSignedXml());
     test.done();
 };
