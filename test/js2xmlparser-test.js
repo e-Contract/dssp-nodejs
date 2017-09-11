0e25c8c817bedc82c1f7efa4cf8c1124d27c8e47
Frank Cornelis <info@e-contract.be>
2017-09-11 08:16:27 +0200

    update dependencies

--- test/js2xmlparser-test.js
+++ test/js2xmlparser-test.js
@@ -1,3 +1,21 @@
+/*
+ * Digital Signature Service Protocol Project.
+ * Copyright (C) 2015-2017 e-Contract.be BVBA.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
 "use strict";
 
 var js2xmlparser = require("js2xmlparser");
@@ -21,7 +39,7 @@ function SecurityTokenReferenceKeyInfoProvider(tokenIdentifier, key) {
                 }
             }
         };
-        var result = js2xmlparser("wsse:SecurityTokenReference", securityTokenReferenceData, {
+        var result = js2xmlparser.parse("wsse:SecurityTokenReference", securityTokenReferenceData, {
             declaration: {
                 include: false
             }
@@ -56,7 +74,7 @@ exports['test js2xmlparser'] = function (test) {
             }
         }
     };
-    var result = js2xmlparser("async:PendingRequest", data);
+    var result = js2xmlparser.parse("async:PendingRequest", data);
     console.log(result);
     var signature = new xmlCrypto.SignedXml();
     var key = crypto.randomBytes(32);
